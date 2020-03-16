a7bc1dcb27e6d1a1c0014c4096bf9bdd6e009494
Jonas Liljestrand <jonas.liljestrand@gmail.com>
2020-03-16 17:48:42 +0100

    index library into database

--- src/component/mod.rs
+++ src/component/mod.rs
@@ -11,8 +11,10 @@ pub struct App {
 impl App {
     pub fn new<T: AsRef<Path>>(path: T) -> Self {
         let (tx, rx) = channel::<Event>();
+        let database = Database::new(path);
+
         let rekordbox_server = Server::new(
-            Database::new(path),
+            database,
             tx,
         );
 
--- src/rekordbox/database.rs
+++ src/rekordbox/database.rs
@@ -1,31 +1,40 @@
-use std::fs::File;
 use std::path::Path;
-use std::sync::{Arc, RwLock};
-use std::io;
+use std::sync::{Arc, RwLock, RwLockWriteGuard};
 
 use crate::rekordbox::Track;
 use crate::library::scan_folder;
 
-struct InnerDatabase {
-    albums: Vec<String>,
-    tracks: Vec<String>,
-    artists: Vec<String>,
+#[derive(Debug)]
+pub enum DatabaseError {
+    Unknown,
 }
 
+#[derive(Debug)]
+struct InnerDatabase {
+    collection: Vec<Track>,
+}
+
+impl InnerDatabase {
+    fn add(&mut self, track: Track) -> Result<(), DatabaseError> {
+        self.collection.push(track);
+
+        Ok(())
+    }
+}
+
+#[derive(Debug)]
 pub struct Database {
-    inner: Arc<RwLock<InnerDatabase>>,
+    inner: RwLock<InnerDatabase>,
 }
 
 impl Database {
     pub fn new<T: AsRef<Path>>(root_folder: T) -> Self {
         let inner_db = InnerDatabase {
-            albums: Vec::new(),
-            tracks: Vec::new(),
-            artists: Vec::new(),
+            collection: vec![],
         };
 
         let database = Self {
-            inner: Arc::new(RwLock::new(inner_db)),
+            inner: RwLock::new(inner_db),
         };
 
         for track in scan_folder(&root_folder) {
@@ -35,7 +44,23 @@ impl Database {
         database
     }
 
-    fn index(&self, track: Track) -> Result<(), io::Error> {
-        Ok(())
+    fn index(&self, track: Track) -> Result<(), DatabaseError> {
+        self.write(|writer| {
+            writer.add(track)
+        })
+    }
+
+    fn tracks(&self) -> Result<Vec<Track>, DatabaseError> {
+        Ok(vec![])
+    }
+
+    fn write<T>(&self, closure: T) -> Result<(), DatabaseError>
+    where
+        T: FnOnce(&mut RwLockWriteGuard<InnerDatabase>) -> Result<(), DatabaseError>
+    {
+        match self.inner.write() {
+            Ok(mut writer) => closure(&mut writer),
+            Err(_err) => Err(DatabaseError::Unknown),
+        }
     }
 }
